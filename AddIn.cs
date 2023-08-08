5e813d982c927b45dd46d98a41afa0190ef821c2
Govert van Drimmelen <govert@dnakode.com>
2023-08-08 22:11:51 +0200

    Add project files.

---
+++ AddIn.cs
@@ -0,0 +1,18 @@
+﻿using ExcelDna.Integration;
+using ExcelDna.IntelliSense;
+
+namespace TestIntelliRc6
+{
+    public class AddIn : IExcelAddIn
+    {
+        public void AutoOpen()
+        {
+            IntelliSenseServer.Install();
+        }
+
+        public void AutoClose()
+        {
+            IntelliSenseServer.Uninstall();
+        }
+    }
+}
---
+++ Functions.cs
@@ -0,0 +1,15 @@
+﻿using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIntelliRc6
+{
+    public static class Functions
+    {
+        [ExcelFunction("Simple hello function")]
+        public static string SayHello(string name) => $"Hello {name}!";
+    }
+}
